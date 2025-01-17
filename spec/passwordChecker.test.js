ad5d9025ca7a52f625377524b8890ade5e851080
Bastien <bastien.joyeau@student.vinci.be>
2025-01-17 16:57:02 +0100

    add first test for 8 caracters

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -2,4 +2,8 @@ const passwordChecker = require('../src/passwordChecker');
 
 describe('Password Checker', () => {
 
+    it('should reject passwords with less than 8 characters', () => {
+        expect(passwordChecker('Short7!')).toBe(false);
+    });
+    
 });
