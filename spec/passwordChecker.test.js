7c7e6c1da46bcd3c7b6354dfa59c1b6d7a142db5
Bastien <bastien.joyeau@student.vinci.be>
2025-01-17 16:58:12 +0100

    add second test valid password

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -5,5 +5,9 @@ describe('Password Checker', () => {
     it('should reject passwords with less than 8 characters', () => {
         expect(passwordChecker('Short7!')).toBe(false);
     });
+
+    it('should accept valid passwords', () => {
+        expect(passwordChecker('Valid123!')).toBe(true);
+    });
     
 });
